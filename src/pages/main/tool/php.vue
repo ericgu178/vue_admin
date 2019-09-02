312c7b185ae5eb1add815889c57867e38a9ef1f4
谷雪健 <15511604615@163.com>
2019-09-02 17:17:22 +0800

    添加php工具页面

--- src/pages/main/tool/php.vue
+++ src/pages/main/tool/php.vue
@@ -20,11 +20,18 @@
 
       	<!-- 下方展示 -->
 		<template v-if="'json_decode_to_string' == index">
-			<div style="width:700px;background:#0f0;float:left">
-asd
+			<div style="width:700px;float:left;margin-top:10px;margin-right:20px">
+				<a-textarea placeholder='格式如下
+请以方括号[]或者花括号{}开始结束
+[{"nav_name":"test","category_id":"categoryProductId1","products":[{"id":11,"goods_name":"829日优惠券商品1","stock":10,"end_effective_time":2147483647,"start_effective_time":0,"purchase_num":10,"agreement":"这是一件自动退款的商品","instructions":"看清楚了再买","goods_pid":27,"skus":[{"goods_spec_id":47,"biz":2333,"shelves_goods_id":46,"property":"22","property_name":[""],"goods_sn":"00001","cost_integral":20,"cost_price":5,"market_integral":1,"market_price":0.01,"goods_number":10,"stock_number":10,"stock_lock":0,"create_time":1567244420,"update_time":1567244420,"delete_time":0,"name_str":"","stock":10}],"sku":[],"property_status":0,"freight_id":0,"original_price":5,"original_integral":20,"market_price":0.01,"market_integral":1,"buy_limit":3,"imgs":[{"id":393,"filepath":"https:\/\/oss.xinliu.org\/uploads\/20190829\/image190829134530566.png"}]}]}]' :rows="40" v-model="form.beforeData"/>
 			</div>
-			<div style="width:700px;background:#0ff;float:left;margin-left:20px;">
-asd
+      		<div style="width:50px;float:left;margin:20% auto;">
+        		<a-button type="primary" @click="toArray">转换成直接可用数组</a-button>
+    			<a-button style="margin-top:10px" @click="copy" v-if="form.visable">复制可用数组</a-button>
+				<a-button style="margin-top:10px" @click="form.visable = false;form.beforeData = '';form.afterData = ''" v-if="form.visable">清空全部</a-button>
+			</div>
+			<div style="width:750px;float:left;margin-left:130px;margin-top:10px;" v-if="form.visable">
+        		<pre>{{form.afterData}}</pre>
 			</div>
 		</template>
 	</div>
@@ -33,9 +40,14 @@ asd
 export default {
 	data () {
     	return {
-			index: 'php',
+      		index: 'php',
+      		form:{
+      		    beforeData:'',
+				afterData:``,
+				visable:false
+      		},
 			items: [{
-				icon: 'tool',
+        	icon: 'tool',
 				index: 'tool',
                 title: '工具',
                 subs: [{
@@ -65,6 +77,34 @@ export default {
 	methods:{
 		titleClick:function(index) {
 			this.index = index
+		},
+		toArray:function() {
+			try {
+				var b = JSON.parse(this.form.beforeData)
+			} catch (error) {
+        		this.$message.error('请填写正确的的json数据')
+        		return;
+			}
+			this.request.request_post(
+				`${this.request.base_url}/admin/tool/jsonToArray`,
+				res=>{
+          			this.form.visable = true
+					this.form.afterData = res.data.data
+				},e=>{
+					this.$message.error('网络错误')
+				},
+				{before:JSON.stringify(b)}
+			)
+		},
+		copy:function() {
+			var oInput = document.createElement("textarea");
+            oInput.value = this.form.afterData;
+            document.body.appendChild(oInput);
+            oInput.select(); // 选择对象
+            document.execCommand("Copy"); // 执行浏览器复制命令
+            oInput.className = "oInput";
+            oInput.style.display = "none";
+            this.$message.success("复制成功");
 		}
 	}
 }
